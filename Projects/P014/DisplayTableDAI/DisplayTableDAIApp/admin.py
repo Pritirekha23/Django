bdf2be1ff0ca2f23a3c143ae28e85b04d916511d
Pritirekha panda <learnpriti23@gmail.com>
2024-05-26 15:14:46 +0530

    P014

--- Projects/P014/DisplayTableDAI/DisplayTableDAIApp/admin.py
+++ Projects/P014/DisplayTableDAI/DisplayTableDAIApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Mobile
 # Register your models here.
+
+# oneway
+# class MobileAdmin(admin.ModelAdmin):
+#     list_display=('id','name','brand','price')
+
+# admin.site.register(Mobile,MobileAdmin)
+
+#by using decorator
+@admin.register(Mobile)
+class MobileAdmin(admin.ModelAdmin):
+    list_display=('id','name','brand','price')
+
